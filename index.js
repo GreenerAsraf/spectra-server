082a9d17ca588f25b7df20095fa83a03adc4bc29
Asraf <asraf.uddin4407@gmail.com>
2022-12-30 12:31:34 -0600

    added

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT||5000;
+
 const app = express()
-const port = 5000
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+const uri = "mongodb+srv://dbuser:JbWGu4f4ZAZ1T4Xl@cluster0.ryc7o3n.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const userCollection = client.db("spectra").collection("users");
+    const commentCollection = client.db("spectra").collection("comments");
+    // create a document to insert
+    const user = {
+      title: "Record of a Shriveled Datum",
+      content: "No bytes, no problem. Just insert a document, in MongoDB",
+    }
+
+    const result = await userCollection.insertOne(user);
+    console.log(`A document was inserted with the _id: ${result}`);
+
+    app.get('/comments', async (req, res) =>{
+      const query = {};
+      const comments = await commentCollection.find(query).toArray();
+      res.send(comments)
+
+    })
+
+
+    app.post('/comments',async (req,res)=>{
+      const comment = req.body;
+      console.log(comment);
+      const result = await commentCollection.insertOne(comment);
+
+    })
+  }
+   finally {
+    
+  }
+}
+run().catch(err => console.log(err) );
 
 app.get('/', (req, res) => {
   res.send('Spectra server is running')
 })
 
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// user: dbuser 
+// pass: JbWGu4f4ZAZ1T4Xl
+
